de9386fb18529920f5af03f3b704a517c145f89b
Giancarlos Salas <giansalex@gmail.com>
2021-01-29 13:52:57 -0500

    Initial Commit

---
+++ cmd/tm-load-test/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/interchainio/tm-load-test/pkg/loadtest"
+)
+
+const appLongDesc = `Load testing application for Tendermint with optional master/slave mode.
+Generates large quantities of arbitrary transactions and submits those 
+transactions to one or more Tendermint endpoints. By default, it assumes that
+you are running the kvstore ABCI application on your Tendermint network.
+
+To run the application in a similar fashion to tm-bench (STANDALONE mode):
+    tm-load-test -c 1 -T 10 -r 1000 -s 250 \
+        --broadcast-tx-method async \
+        --endpoints ws://tm-endpoint1.somewhere.com:26657/websocket,ws://tm-endpoint2.somewhere.com:26657/websocket
+
+To run the application in MASTER mode:
+    tm-load-test \
+        master \
+        --expect-slaves 2 \
+        --bind localhost:26670 \
+        --shutdown-wait 60 \
+        -c 1 -T 10 -r 1000 -s 250 \
+        --broadcast-tx-method async \
+        --endpoints ws://tm-endpoint1.somewhere.com:26657/websocket,ws://tm-endpoint2.somewhere.com:26657/websocket
+
+To run the application in SLAVE mode:
+    tm-load-test slave --master localhost:26680
+
+NOTES:
+* MASTER mode exposes a "/metrics" endpoint in Prometheus plain text format
+  which shows total number of transactions and the status for the master and
+  all connected slaves.
+* The "--shutdown-wait" flag in MASTER mode is specifically to allow your 
+  monitoring system some time to obtain the final Prometheus metrics from the
+  metrics endpoint.
+* In SLAVE mode, all load testing-related flags are ignored. The slave always 
+  takes instructions from the master node it's connected to.
+`
+
+func main() {
+	loadtest.Run(&loadtest.CLIConfig{
+		AppName:              "tm-load-test",
+		AppShortDesc:         "Load testing application for Tendermint kvstore",
+		AppLongDesc:          appLongDesc,
+		DefaultClientFactory: "kvstore",
+	})
+}
---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/interchainio/tm-load-test
+
+go 1.15
+
+require (
+	github.com/gorilla/websocket v1.4.2
+	github.com/prometheus/client_golang v1.8.0
+	github.com/satori/go.uuid v1.2.0
+	github.com/sirupsen/logrus v1.6.0
+	github.com/spf13/cobra v1.1.1
+	github.com/tendermint/tendermint v0.34.3
+	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9
+)
